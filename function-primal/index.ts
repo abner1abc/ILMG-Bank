d6d165c3f3285ee65b8b3c2cab85beab05b620aa
Abner Valverde Cunha <abnercunha2247@gmail.com>
2023-11-09 05:58:27 -0300

    update ILMG Bank, add cancel and resolve bugs

--- function-primal/index.ts
+++ function-primal/index.ts
@@ -1,11 +1,19 @@
+// Criar uma conta
+// Consultar o saldo da conta
+// Depositar na conta
+// Sacar na conta
+// Encerrar a conta
+// como realizar transferências entre contas
+
 interface Iconta{
     nomeTitular: string;
     saldo?: number;
 }
 
-class conta{
+class Conta{
     private nomeTitular: string;
     private saldo: number;
+    private ativa: boolean = true;
 
     constructor({
         nomeTitular,
@@ -18,24 +26,57 @@ class conta{
 
     meuSaldo = (): number => this.saldo
 
-    depositar = (valor: number) => {
+    depositar = (valor: number): void | boolean => {
+        if(!this.ativa){
+            console.log('Conta desativada, impossível realizar depósito')
+            return this.ativa
+        }
         this.saldo = this.saldo + valor
         console.log(`Você depositou ${valor}. Seu novo saldo é ${this.saldo}`)
     }
+
+    sacar = (valor: number): boolean =>{ 
+        if(!this.ativa){
+            console.log('Conta desativada, impossível realizar saque')
+            return this.ativa
+        }
+        if(valor <= this.saldo){
+            this.saldo -= valor
+            console.log(`Você sacou ${valor}. Seu novo saldo é ${this.saldo}`)
+            return this.ativa 
+        }
+
+        console.log(`Saldo insuficiente.Você quer sacar ${valor} Seu saldo atual é ${this.saldo}`)
+        return this.ativa 
+        
+    }
+
+    cancelarConta = () => {
+        if(this.saldo === 0 ){
+            this.ativa = false
+            console.log('Conta Desativada')
+            return this.ativa
+        }
+        
+        console.log(`Impossível desativar conta de ${this.nomeTitular}. Conta com pendência ativa!`)
+    }
 }
 
-const contaA: conta = new conta({nomeTitular: 'Abner'})
+const conta: Conta = new Conta({nomeTitular: 'Abner'})
 //const contaB: conta = new conta({nomeTitular: 'Remaldo',saldo:10})
 
-console.log('Antes do depósito',contaA.meuSaldo())
-contaA.depositar(100)
+conta.cancelarConta()
 
-console.log('Depois do depósito',contaA.meuSaldo())
+conta.depositar(10)
+
+
+//console.log('Antes do depósito',contaA.meuSaldo())
+//contaA.depositar(100)
+
+//console.log('Depois do depósito',contaA.meuSaldo())
+
+//contaA.sacar(120)
+
+//console.log('Depois do saque',contaA.meuSaldo())
 
 //console.log(contaB)
-
-// Criar uma conta
-// Consultar o saldo da conta
-// Depositar na conta
-// Sacar na conta
-// Encerrar a conta
